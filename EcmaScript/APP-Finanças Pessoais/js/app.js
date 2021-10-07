c2670da99f325e38047a804e8f9aa94300263ac5
Eline Farias <elinefarias33@gmail.com>
2021-10-07 14:01:25 -0300

    funcao carrega lista despesasinseridas

--- EcmaScript/APP-Finanças Pessoais/js/app.js
+++ EcmaScript/APP-Finanças Pessoais/js/app.js
@@ -1,4 +1,4 @@
-class Despesas {
+class Despesa {
         constructor(ano,mes,dia,tipo,descricao,valor) {
                 this.ano = ano
                 this.mes = mes
@@ -34,11 +34,34 @@ class Bd {
             localStorage.setItem(id, JSON.stringify(d))
             localStorage.setItem('id', id)
       }
+      recuperarTodosRegistros() {
+             //array de despesas
+		let despesas = Array()
+
+		let id = localStorage.getItem('id')
+
+		//recuperar todas as despesas cadastradas em localStorage
+		for(let i = 1; i <= id; i++) {
+
+			//recuperar a despesa
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			//existe a possibilidade de haver índices que foram pulados/removidos
+			//nestes casos nós vamos pular esses índices
+			if(despesa === null) {
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+
+		return despesas
+	}
 }
 
-let bd = new Bd()
+let  bd  =  new Bd()
 
-function cadastrarDespesas() {
+function cadastrarDespesa() {
         let  ano =  document.getElementById('ano')
         let  mes  =  document.getElementById('mes')
         let  dia  =  document.getElementById('dia')
@@ -46,7 +69,7 @@ function cadastrarDespesas() {
         let  descricao  =  document.getElementById('descricao')
         let  valor  =  document.getElementById('valor')
         
-        let despesas  = new Despesas(
+        let despesa  = new Despesa(
               ano.value,
               mes.value,
               dia.value,
@@ -54,8 +77,8 @@ function cadastrarDespesas() {
               descricao.value,
               valor.value
         )
-        if(despesas.validarDados()) {
-                bd.gravar(despesas)
+        if(despesa.validarDados()) {
+                bd.gravar(despesa)
                 document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
                 document.getElementById('modal_titulo_div').className = 'modal-header text-success'
                 document.getElementById('modal_conteudo').innerHTML = 'Despesas Cadastradas com Sucesso'
@@ -71,3 +94,37 @@ function cadastrarDespesas() {
                 $('#modalRegistraDespesas').modal('show')
         }
 }
+function carregaListaDespesas(){
+        let  despesas  =  Array()
+        despesas  =  bd.recuperarTodosRegistros()
+        let listaDespesas = document.getElementById("listaDespesas")
+
+	despesas.forEach(function(d){
+
+		//Criando a linha (tr)
+		var linha = listaDespesas.insertRow();
+
+		//Criando as colunas (td)
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
+
+		//Ajustar o tipo
+		switch(d.tipo){
+			case '1': d.tipo = 'Alimentação'
+				break
+			case '2': d.tipo = 'Educação'
+				break
+			case '3': d.tipo = 'Lazer'
+				break
+			case '4': d.tipo = 'Saúde'
+				break
+			case '5': d.tipo = 'Transporte'
+				break
+			
+		}
+		linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+		console.log(d)
+	})
+
+ }
