371f9c19fa9a19a0d550627e99c694f70598f5b9
Eline Farias <elinefarias33@gmail.com>
2021-10-07 21:44:39 -0300

    APP finanças pessoais concluidas

--- EcmaScript/APP-Finanças Pessoais/js/app.js
+++ EcmaScript/APP-Finanças Pessoais/js/app.js
@@ -51,12 +51,40 @@ class Bd {
 			if(despesa === null) {
 				continue
 			}
-
+                        despesa.id = i
 			despesas.push(despesa)
 		}
 
 		return despesas
 	}
+                pesquisar(despesa) {
+                        let despesasFiltradas = Array()
+                        despesasFiltradas = this.recuperarTodosRegistros()
+                        console.log(despesasFiltradas)
+                        console.log(despesa)
+                        if(despesa.ano !=  '') {
+                                despesasFiltradas = despesasFiltradas.filter(d =>d.ano == despesa.ano)
+                        }
+                        if(despesa.mes !=  '') {
+                                despesasFiltradas = despesasFiltradas.filter(d =>d.mes == despesa.mes)
+                        }
+                        if(despesa.dia != '') {
+                                despesasFiltradas = despesasFiltradas.filter(d =>d.dia == despesa.dia)
+                        }
+                        if(despesa.tipo != '') {
+                                despesasFiltradas = despesasFiltradas.filter(d =>d.tipo == despesa.tipo)
+                        }
+                        if(despesa.descricao !='') {
+                                despesasFiltradas = despesasFiltradas.filter(d =>d.descricao == despesa.descricao)
+                        } 
+                        if(despesa.valor !='') {
+                                despesasFiltradas = despesasFiltradas.filter(d =>d.valor == despesa.valor)
+                        }
+                        return despesasFiltradas
+                }
+                remover(id) {
+                        localStorage.removeItem(id)
+                }
 }
 
 let  bd  =  new Bd()
@@ -101,37 +129,81 @@ function cadastrarDespesa() {
                 $('#modalRegistraDespesas').modal('show')
         }
 }
-function carregaListaDespesas(){
-        let  despesas  =  Array()
-        despesas  =  bd.recuperarTodosRegistros()
-        let listaDespesas = document.getElementById("listaDespesas")
 
-	despesas.forEach(function(d){
+function carregaListaDespesas(despesas = Array(), filtro = false) {
 
-		//Criando a linha (tr)
-		var linha = listaDespesas.insertRow();
+        if(despesas.length == 0 && filtro == false){
+                    despesas = bd.recuperarTodosRegistros() 
+            }
+            
+    
+            /*
+    
+            <tr>
+                    <td>15/03/2018</td>
+                    <td>Alimentação</td>
+                    <td>Compras do mês</td>
+                    <td>444.75</td>
+            </tr>
+    
+            */
+    
+            let listaDespesas = document.getElementById("listaDespesas")
+        listaDespesas.innerHTML = ''
+            despesas.forEach(function(d){
+    
+                    //Criando a linha (tr)
+                    var linha = listaDespesas.insertRow();
+    
+                    //Criando as colunas (td)
+                    linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
+    
+                    //Ajustar o tipo
+                    switch(d.tipo){
+                            case '1': d.tipo = 'Alimentação'
+                                    break
+                            case '2': d.tipo = 'Educação'
+                                    break
+                            case '3': d.tipo = 'Lazer'
+                                    break
+                            case '4': d.tipo = 'Saúde'
+                                    break
+                            case '5': d.tipo = 'Transporte'
+                                    break
+                            
+                    }
+                    linha.insertCell(1).innerHTML = d.tipo
+                    linha.insertCell(2).innerHTML = d.descricao
+                    linha.insertCell(3).innerHTML = d.valor
+                    //botão excluir
+                    let btn = document.createElement("button")
+                    btn.className  =  'btn btn-danger'
+                    btn.innerHTML  = '<i class = "fas fa-times"></i> '
+                    btn.id = `id_despesa_${d.id}`
+                    btn.onclick = function() {
+                                let id  =  this.id.replace('id_despesa_', '')
+                                bd.remover(id)
+                                window.location.reload()
+                    }
+                    linha.insertCell(4).append(btn)
+                    console.log(d)
+            })
+    
+     }
+    
+ function pesquisarDespesa(){
+	 
+	let ano  = document.getElementById("ano").value
+	let mes = document.getElementById("mes").value
+	let dia = document.getElementById("dia").value
+	let tipo = document.getElementById("tipo").value
+	let descricao = document.getElementById("descricao").value
+	let valor = document.getElementById("valor").value
 
-		//Criando as colunas (td)
-		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
 
-		//Ajustar o tipo
-		switch(d.tipo){
-			case '1': d.tipo = 'Alimentação'
-				break
-			case '2': d.tipo = 'Educação'
-				break
-			case '3': d.tipo = 'Lazer'
-				break
-			case '4': d.tipo = 'Saúde'
-				break
-			case '5': d.tipo = 'Transporte'
-				break
-			
-		}
-		linha.insertCell(1).innerHTML = d.tipo
-		linha.insertCell(2).innerHTML = d.descricao
-		linha.insertCell(3).innerHTML = d.valor
-		console.log(d)
-	})
+	let despesas = bd.pesquisar(despesa)
+	 
+	this.carregaListaDespesas(despesas, true)
 
  }
