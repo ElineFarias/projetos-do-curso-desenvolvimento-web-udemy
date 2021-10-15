00e725877cf480fe793f532006afdf2efb9e0d4b
Eline Farias <elinefarias33@gmail.com>
2021-10-15 01:10:06 -0300

    Adicionada a pasta dos arquivos php

---
+++ php/exercicios/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset= "utf-8">
+    <title>Exercicios PHP</title>
+  </head>
+  <body>
+     <?php
+       $idade = 25;
+       $peso = 59;
+       if($idade >=16 && $idade<=69 && $peso >= 50) {
+         echo "Atende aos requisitos";
+       } else {
+         echo "Não atende aos requisitos";
+       }
+      ?>
+  </body>
+</html> 
